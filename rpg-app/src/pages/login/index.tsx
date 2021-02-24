e1bfca2ba23da2bb070a1c147dd234dcca05a57d
Joao Victor Brandao Silverio <Joao.silverio@venturus.org.br>
2021-02-23 21:32:08 -0300

    Login page start

--- rpg-app/src/pages/login/index.tsx
+++ rpg-app/src/pages/login/index.tsx
@@ -1,32 +1,93 @@
 import React from "react";
-import { Container, Box, Paper, Typography } from "@material-ui/core";
-import imagemLogin from "../../components/images/fundo.png";
+import {
+  Container,
+  Box,
+  Paper,
+  Typography,
+  Grid,
+  TextField,
+  Button,
+} from "@material-ui/core";
 import { useStyles } from "./styles";
 
 const Login = () => {
   const styles = useStyles();
   return (
     <Container
-      style={{ backgroundImage: `url(${imagemLogin})` }}
+      style={{
+        background: "red",
+        display: "flex",
+      }}
       className={styles.container}
     >
-      <Paper>
-        <Box>
-          <Typography>Login</Typography>
-        </Box>
-        <Box>
-          <Box>Seu nome</Box>
-          <Box>Sua senha</Box>
-          <Box>
-            <Box>Manter</Box>
-            <Box>Check</Box>
+      <Grid item xl={7} sm={7} lg={7}>
+        Outras coisas
+      </Grid>
+      <Grid
+        item
+        xl={5}
+        sm={5}
+        lg={5}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Paper style={{ width: "60%", height: "80%" }}>
+          <Box
+            style={{
+              display: "flex",
+              height: "18%",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h6">Faça seu login</Typography>
           </Box>
-          <Box>
-            <Box>Esqueceu</Box>
-            <Box>Register</Box>
+          <Box style={{ height: "82%" }}>
+            <Box
+              style={{
+                height: "50%",
+                padding: "32px 32px 0px 32px",
+              }}
+            >
+              <TextField
+                style={{ marginBottom: "16px", width: "100%" }}
+                label="email"
+                variant="outlined"
+              />
+              <TextField
+                label="senha"
+                variant="outlined"
+                style={{ width: "100%" }}
+              />
+              <Box>
+                <Button
+                  style={{
+                    border: "1px solid black",
+                    marginTop: "16px",
+                    width: "100%",
+                  }}
+                >
+                  Logar
+                </Button>
+              </Box>
+            </Box>
+            <Box
+              style={{
+                height: "25%",
+                display: "grid",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Box>Esqueceu sua senha?</Box>
+              <Box>Criar nova conta</Box>
+            </Box>
           </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </Grid>
     </Container>
   );
 };
